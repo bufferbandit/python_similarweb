ef927cea07e321d0abbf6f4cf2127e56d82e8f2a
kernelland <kernelland@protonmail.ch>
2020-08-27 22:01:44 +0200

    Update similarweb.py

--- similarweb.py
+++ similarweb.py
@@ -1,9 +1,7 @@
 from urllib.parse import urlparse
 from multiprocessing import Pool
-import statistics
-import requests
-import sys
-
+from threading import Lock
+import statistics,requests,sys
 
 GREEN = '\033[1;32;48m'
 RED = '\033[1;31;48m'
@@ -11,6 +9,8 @@ UNDERLINE = '\033[4;37;48m'
 END = '\033[1;37;0m'
 
 api_url = "https://data.similarweb.com/api/v1/data?domain="
+
+lock = Lock()
 
 def num_to_word(number):
     nstr = str(number)
@@ -24,17 +24,19 @@ def num_to_word(number):
 
 
 def process_thread(domain):
-    response = requests.get(api_url + domain)
-    json_response = response.json()
-    if json_response:
-        EstimatedMonthlyVisitsDict = json_response["EstimatedMonthlyVisits"]
+    response = requests.get(api_url + domain).json()
+    if response:
+        EstimatedMonthlyVisitsDict = response["EstimatedMonthlyVisits"]
         avg_visitors = round(statistics.mean(
             EstimatedMonthlyVisitsDict.values()))
+        lock.acquire()
         print(f"{GREEN}[+]{END} Found that domain: {domain} has: {UNDERLINE}{num_to_word(avg_visitors)}{END} ({avg_visitors:,}) avg. visitors".replace(
             ",", f"{GREEN},{END}"))
+        lock.release()
     else:
+        lock.acquire()
         print(f"{RED}[!]{END} No data found for domain: {domain}")
-
+        lock.release()
 
 if __name__ == "__main__":
     pool = Pool(20)
